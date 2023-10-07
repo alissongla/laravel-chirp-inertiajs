3d2a721a7c61f9cbb2f3ce7d55dde2f291f9a9aa
alissongla <alisson.campos.dev@gmail.com>
2023-10-07 18:59:15 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Laravel Chirp - InertiaJS - Vue
+Este e um projeto de estudo, onde estou aprendendo a utilizar o InertiaJS com VueJS e Laravel.
---
+++ app/Models/Chirp.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class
+    ];
+
+    /**
+     * Get the user that owns the chirp.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
